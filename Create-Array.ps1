8427b44313c2efa66b7b4db9c7828ab00a7426fd
ivanovash <34724872+ivanovash@users.noreply.github.com>
2017-12-27 18:00:02 -0800

    creation

---
+++ Create-Array.ps1
@@ -0,0 +1,9 @@
+﻿function Create-Array{
+	$OutTable = @()
+	Do{
+		$entry = read-host "Enter string, Blank to end"
+		If($entry -ne ''){$OutTable += $entry}
+	}
+	While($entry -ne '')
+	return $OutTable
+}
